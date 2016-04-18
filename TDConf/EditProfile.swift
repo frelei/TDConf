4de81dc8d1ccde6b5b1e71bb894301e587064289
Rodrigo Freitas Leite <rodrigo.freitas.leite@gmail.com>
2016-04-18 17:41:52 -0300

    add edit profile

--- TDConf/EditProfile.swift
+++ TDConf/EditProfile.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
-class EditProfile: NSObject {
+class EditProfile: UITableViewController, UITextViewDelegate, UITextFieldDelegate {
 
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var btnProfileImg: UIButton!
+    @IBOutlet weak var txtFieldName: UITextField!
+    @IBOutlet weak var txtFieldEmail: UITextField!
+    @IBOutlet weak var txvBio: UITextView!
+    
+    // MARK: VC Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        txvBio.delegate = self
+        txtFieldName.delegate = self
+        txtFieldEmail.delegate = self
+    }
+    
+    // MARK: TEXTFIELD DELEGATE
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    // MARK: TEXTVIEW DELEGATE
+    func textViewDidChange(textView: UITextView) {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    // MARK: IBACTION
+    @IBAction func btnDoneClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true) { 
+            
+        }
+    }
+    
+    
+    @IBAction func btnCancelCliked(sender: AnyObject) {
+        
+    }
+    
+    
 }
--- TDConf/ProfileVC.swift
+++ TDConf/ProfileVC.swift
@@ -44,14 +44,8 @@ class ProfileVC: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func editBtnClicked(sender: AnyObject) {
+        self.performSegueWithIdentifier("goToEditProfileSegue", sender: nil)
     }
-    */
 
 }
